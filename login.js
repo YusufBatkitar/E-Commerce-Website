76b48017a2fbc3a3f3176a3891ab2bc83d4041b6
YusufBatkitar <211005023@bakircay.edu.tr>
2024-12-30 19:11:32 +0300

    a

--- login.js
+++ login.js
@@ -9,18 +9,17 @@ document.addEventListener('DOMContentLoaded', function() {
         const username = usernameInput.value;
         const password = passwordInput.value;
 
-        // Mevcut kullanıcıları getir
+        
         const users = JSON.parse(localStorage.getItem('users')) || [];
 
-        // Kullanıcı bilgilerini kontrol et
+       
         const user = users.find(user => user.username === username && user.password === password);
 
         if (user) {
-            // Kullanıcı doğrulandı
             localStorage.setItem('loggedIn', 'true'); 
-            localStorage.setItem('user', JSON.stringify(user)); // Kullanıcı bilgilerini kaydediyoruz
-
-            window.location.href = 'index.html';  // Başarılı girişten sonra index.html'e yönlendir
+            localStorage.setItem('user', JSON.stringify(user));
+            localStorage.setItem('fromLogin', 'true'); 
+            window.location.href = 'index.html';  
         } else {
             alert('Invalid username or password. Please try again.');
         }
